e13be49637c6181c88d9c6892d219ea70e3f0eb8
udaykene <udaykene96@gmail.com>
2026-02-01 19:32:58 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AnimatedSvgBackground from "./components/AnimatedSvgBackground";
+const App = () => {
+  return (
+    <div>
+      <AnimatedSvgBackground />
+      <div>
+        <h1>App</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
