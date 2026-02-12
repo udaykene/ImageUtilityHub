85d904ca7f39a56f271f7ee117220bf3c4eae3b8
udaykene <udaykene96@gmail.com>
2026-02-12 22:50:39 +0530

    Cleaned the backend Folder

--- backend/src/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const upload = require('../middlewares/upload');
-const { uploadFile } = require('../controllers/uploadController');
-
-// Upload single file
-router.post('/', upload.single('image'), uploadFile);
-
-module.exports = router;
